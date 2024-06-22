58fc5c9f230ba2a1ab7524b1f76a538d4f7e477a
TushitPalamkar <143091687+TushitPalamkar@users.noreply.github.com>
2024-06-22 15:13:47 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const app=express()
 app.use(express.json())
 app.use(cors())
 app.use(router)
-const PORT= 5000
+const PORT= process.env.port||5000
 mongoose.connect(process.env.MONGODB_URL)
 .then(console.log('Connection is successfull')).catch((error)=>{console.log(error)})
 app.listen(PORT,()=>{console.log(`Server is listening on port ${PORT}`)})
